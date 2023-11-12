c037f52693bd3e03736d17ae093d61789dd0ed77
Caesar <casearx@gmail.com>
2023-11-12 20:03:58 +0800

    Update and rename 数据库备份脚本.sh to 数据库备份脚本（带远程登录）.sh

--- 数据库备份脚本（带远程登录）.sh
+++ 数据库备份脚本（带远程登录）.sh
@@ -1,12 +1,12 @@
 set -e
 name="数据库备份_$(date +%Y-%m-%d_%H-%M-%S).sql"
-bendi=/var/backups/mysql
-yuancheng=192.168.236.30::backup
-user=root
-password='MySQL@329958'
-sql_host=localhost
-if [ ! -d "$bendi" ]; then 
-  mkdir -p "$bendi"        
+bendi=/var/backups/mysql    #定义本地路径
+yuancheng=192.168.236.30::backup   #定义远程路径
+user=root    #定义账号
+password='MySQL@329958'  #定义密码
+sql_host=localhost   #定义位置
+if [ ! -d "$bendi" ]; then   #-d检查目录是否存在
+  mkdir -p "$bendi"        #如果不存在则使用mkdir创建
   echo "备份文件夹 $bendi 不存在，已创建该文件夹。"
 fi
 bak(){
